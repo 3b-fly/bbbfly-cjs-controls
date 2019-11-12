204ed29ea8531396162285ad834890c2be0c3228
3b-fly <member@3b-fly.eu>
2019-11-12 18:49:54 +0100

    Removed deprecated "bbbfly.CheckBox" and "bbbfly.RadioButton" controls.

--- build/debug/base/button.js
+++ build/debug/base/button.js
@@ -509,22 +509,10 @@ bbbfly.Button = function(def,ref,parent){
   if(bbbfly.hint){bbbfly.hint.Hintify(def);}
   return ngCreateControlAsType(def,'ngButton',ref,parent);
 };
-bbbfly.CheckBox = function(def,ref,parent){
-  def = bbbfly.button.NormalizeDef(def);
-  if(bbbfly.hint){bbbfly.hint.Hintify(def);}
-  return ngCreateControlAsType(def,'ngCheckBox',ref,parent);
-};
-bbbfly.RadioButton = function(def,ref,parent){
-  def = bbbfly.button.NormalizeDef(def);
-  if(bbbfly.hint){bbbfly.hint.Hintify(def);}
-  return ngCreateControlAsType(def,'ngRadioButton',ref,parent);
-};
 ngUserControls = ngUserControls || new Array();
 ngUserControls['bbbfly_strl_button'] = {
   OnInit: function(){
     ngRegisterControlType('bbbfly.Btn',bbbfly.Btn);
     ngRegisterControlType('bbbfly.Button',bbbfly.Button);
-    ngRegisterControlType('bbbfly.CheckBox',bbbfly.CheckBox);
-    ngRegisterControlType('bbbfly.RadioButton',bbbfly.RadioButton);
   }
 };
--- src/base/button.js
+++ src/base/button.js
@@ -739,59 +739,11 @@ bbbfly.Button = function(def,ref,parent){
   return ngCreateControlAsType(def,'ngButton',ref,parent);
 };
 
-/**
- * @class
- * @type control
- * @extends ngCheckBox
- * @implements bbbfly.hint.Hintified
- * @deprecated will be replaced by {@link bbbbfly.Btn}
- *
- * @description
- *   Implements {@link bbbfly.hint.Hintified|Hintified} interface
- *   if {@link package:hint|hint} package is inluded.
- *
- * @inpackage button
- *
- * @param {ngControl.Definition} [def=undefined] - Descendant definition
- * @param {object} [ref=undefined] - Reference owner
- * @param {object|string} [parent=undefined] - Parent DIV element or it's ID
- */
-bbbfly.CheckBox = function(def,ref,parent){
-  def = bbbfly.button.NormalizeDef(def);
-  if(bbbfly.hint){bbbfly.hint.Hintify(def);}
-  return ngCreateControlAsType(def,'ngCheckBox',ref,parent);
-};
-
-/**
- * @class
- * @type control
- * @extends ngRadioButton
- * @implements bbbfly.hint.Hintified
- * @deprecated will be replaced by {@link bbbbfly.Btn}
- *
- * @description
- *   Implements {@link bbbfly.hint.Hintified|Hintified} interface
- *   if {@link package:hint|hint} package is inluded.
- *
- * @inpackage button
- *
- * @param {ngControl.Definition} [def=undefined] - Descendant definition
- * @param {object} [ref=undefined] - Reference owner
- * @param {object|string} [parent=undefined] - Parent DIV element or it's ID
- */
-bbbfly.RadioButton = function(def,ref,parent){
-  def = bbbfly.button.NormalizeDef(def);
-  if(bbbfly.hint){bbbfly.hint.Hintify(def);}
-  return ngCreateControlAsType(def,'ngRadioButton',ref,parent);
-};
-
 /** @ignore */
 ngUserControls = ngUserControls || new Array();
 ngUserControls['bbbfly_strl_button'] = {
   OnInit: function(){
     ngRegisterControlType('bbbfly.Btn',bbbfly.Btn);
     ngRegisterControlType('bbbfly.Button',bbbfly.Button);
-    ngRegisterControlType('bbbfly.CheckBox',bbbfly.CheckBox);
-    ngRegisterControlType('bbbfly.RadioButton',bbbfly.RadioButton);
   }
 };
