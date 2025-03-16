23fec5ba2973dd594fde5bcae5bd099c6cc012b6
3b-fly <3b-fly@users.noreply.github.com>
2025-03-16 08:36:13 +0100

    Added "bbbfly.EditBox" control.

--- src/base/edit.js
+++ src/base/edit.js
@@ -11,6 +11,8 @@ var bbbfly = bbbfly || {};
 /** @ignore */
 bbbfly.edit = {};
 /** @ignore */
+bbbfly.editbox = {};
+/** @ignore */
 bbbfly.memo = {};
 
 /** @ignore */
@@ -302,15 +304,6 @@ bbbfly.Memo = function(def,ref,parent){
   return ngCreateControlAsType(def,'ngMemo',ref,parent);
 };
 
-/** @ignore */
-ngUserControls = ngUserControls || new Array();
-ngUserControls['bbbfly_edit'] = {
-  OnInit: function(){
-    ngRegisterControlType('bbbfly.Edit',bbbfly.Edit);
-    ngRegisterControlType('bbbfly.Memo',bbbfly.Memo);
-  }
-};
-
 /**
  * @interface Button
  * @memberOf bbbfly.Edit
@@ -336,4 +329,235 @@ ngUserControls['bbbfly_edit'] = {
  * @extends ngControl.Definition
  *
  * @description Memo control definition
+ */
+
+/** @ignore */
+bbbfly.editbox._setAlt = function(alt,update){
+  if(!String.isString(alt) && (alt !== null)){return;}
+
+  if(alt !== this.Alt){
+    this.Alt = alt;
+
+    if(!Boolean.isBoolean(update) || update){
+      this.Update();
+    }
+  }
+};
+
+/** @ignore */
+bbbfly.editbox._setText = function(text,update){
+  if(!String.isString(text) && (text !== null)){return;}
+
+  if(text !== this.Text){
+    this.Text = text;
+
+    if(!Boolean.isBoolean(update) || update){
+      this.Update();
+    }
+  }
+};
+
+/** @ignore */
+bbbfly.editbox._getAlt = function(){
+  if(String.isString(this.AltRes)){
+    return ngTxt(this.AltRes);
+  }
+  else if(String.isString(this.Alt)){
+    return this.Alt;
+  }
+  return null;
+};
+
+/** @ignore */
+bbbfly.editbox._getText = function(){
+  if(String.isString(this.Text)){
+    return this.Text;
+  }
+  return null;
+};
+
+/** @ignore */
+bbbfly.editbox._getButtons = function(){
+  return Object.isObject(this._Buttons) ? this._Buttons : {};
+};
+
+/** @ignore */
+bbbfly.editbox._getButton = function(buttonId){
+  var btns = this.GetButtons();
+  var btn = btns[buttonId];
+
+  return Object.isObject(btn) ? btn : null;
+};
+
+
+/** @ignore */
+bbbfly.editbox._doCreate = function(def,ref,node){
+  this.DoCreate.callParent(def,ref,node);
+  if(!Object.isObject(this._Buttons)){return;}
+
+  if(Object.isObject(def.Buttons)){
+    for(var btnId in def.Buttons){
+      var btnDef = def.Buttons[btnId];
+
+      if(Object.isObject(btnDef)){
+        if(Object.isObject(this.ButtonDef)){
+          ng_MergeDef(btnDef,this.ButtonDef);
+        }
+
+        this._Buttons[btnId] = this.CreateControl(btnDef);
+      }
+    }
+  }
+};
+
+/**
+ * @class
+ * @type control
+ * @extends bbbfly.Wrapper
+ * @implements bbbfly.hint.Hintified
+ *
+ * @description
+ *   Basic EditBox Control.
+ *   Implements {@link bbbfly.hint.Hintified|Hintified} interface
+ *   if {@link package:hint|hint} package is inluded.
+ *
+ * @inpackage edit
+ *
+ * @param {bbbfly.EditBox.Definition} [def=undefined] - Descendant definition
+ * @param {object} [ref=undefined] - Reference owner
+ * @param {object|string} [parent=undefined] - Parent DIV element or it's ID
+ *
+ * @property {object} WrapperOptions
+ * @property {bbbfly.Wrapper.orientation} WrapperOptions.Orientation=horizontal
+ * @property {boolean} WrapperOptions.TrackChanges=true
+ *
+ * @property {bbbfly.Button.Definition} ButtonDef - Definition shared by all buttons
+ *
+ * @property {string} [Alt=null] - Alt string
+ * @property {string} [AltRes=null] - Alt  resource ID
+ *
+ * @property {string} [Text=null] - Text string
+ * @property {bbbfly.EditBox.textalign} [TextAlign=left]
+ */
+bbbfly.EditBox = function(def,ref,parent){
+  def = def || {};
+
+  ng_MergeDef(def,{
+    Buttons: null,
+    
+    Data: {
+      WrapperOptions: {
+        Orientation: bbbfly.Wrapper.orientation.horizontal,
+        TrackChanges: true
+      },
+
+      ButtonDef: {
+        Type: 'bbbfly.Button'
+      },
+
+      Alt: null,
+      AltRes: null,
+
+      Text: null,
+      TextAlign: bbbfly.EditBox.textalign.left,
+
+      /** @private */
+      _Buttons: {}
+    },
+    Events: {
+
+    },
+    Methods: {
+      /** @private */
+      DoCreate: bbbfly.editbox._doCreate,
+
+      /**
+       * @function
+       * @name SetAlt
+       * @memberof bbbfly.EditBox#
+       *
+       * @param {string|null} alt
+       * @param {boolean} [update=true]
+       */
+      SetAlt: bbbfly.editbox._setAlt,
+      /**
+       * @function
+       * @name SetText
+       * @memberof bbbfly.EditBox#
+       *
+       * @param {string|null} text
+       * @param {boolean} [update=true]
+       */
+      SetText: bbbfly.editbox._setText,
+
+      /**
+       * @function
+       * @name GetAlt
+       * @memberof bbbfly.EditBox#
+       *
+       * @return {string|null}
+       */
+      GetAlt: bbbfly.editbox._getAlt,
+      /**
+       * @function
+       * @name GetText
+       * @memberof bbbfly.EditBox#
+       *
+       * @return {string|null}
+       */
+      GetText: bbbfly.editbox._getText,
+
+      /**
+       * @function
+       * @name GetButtons
+       * @memberof bbbfly.EditBox#
+       *
+       * @return {object}
+       */
+      GetButtons: bbbfly.editbox._getButtons,
+      /**
+       * @function
+       * @name GetButton
+       * @memberof bbbfly.EditBox#
+       *
+       * @param {string} buttonId - Required button's id
+       * @return {bbbfly.Button|null} - Button with passes id
+       */
+      GetButton: bbbfly.editbox._getButton,
+    }
+  });
+
+  if(bbbfly.hint){bbbfly.hint.Hintify(def);}
+  return ngCreateControlAsType(def,'bbbfly.Wrapper',ref,parent);
+};
+
+/**
+ * @enum {integer}
+ * @description
+ *   Possible values for {@link bbbfly.EditBox|bbbfly.EditBox.TextAlign}
+ */
+bbbfly.EditBox.textalign = {
+  left: 1,
+  center: 2,
+  right: 3
+};
+
+/** @ignore */
+ngUserControls = ngUserControls || new Array();
+ngUserControls['bbbfly_edit'] = {
+  OnInit: function(){
+    ngRegisterControlType('bbbfly.Edit',bbbfly.Edit);
+    ngRegisterControlType('bbbfly.EditBox',bbbfly.EditBox);
+    ngRegisterControlType('bbbfly.Memo',bbbfly.Memo);
+  }
+};
+
+/**
+ * @interface Definition
+ * @memberOf bbbfly.EditBox
+ * @extends bbbfly.Wrapper.Definition
+ *
+ * @description EditBox control definition
+ *
+ * @property {object} [Buttons=null] - Define editbox buttons
  */
